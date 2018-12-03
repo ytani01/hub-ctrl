2f24e7ac94185b25748431ed604d5a0704d2762e
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2018-12-04 02:52:09 +0900

    initial commit

---
+++ hub-ctrl.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+MYNAME=`basename $0`
+
+BINDIR=${HOME}/bin
+
+CMD="${BINDIR}/hub-ctrl"
+OPT1="-h 0 -P 2 -p"
+OPT_SW="1"
+
+if [ "$1" = "" ]; then
+	echo "usage: ${MYNAME} on|off"
+	exit 1
+fi
+
+if [ "$1" = "off" ]; then
+	OPT_SW="0"
+fi
+
+CMDLINE="sudo ${CMD} ${OPT1} ${OPT_SW}"
+echo ${CMDLINE}
+exec ${CMDLINE}
